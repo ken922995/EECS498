14732b39e5b6f6ce489d12af66baa481f702d0b2
ken922995 <ken922995@gmail.com>
2020-09-21 09:20:43 -0700

    Q5

--- HW1.py
+++ HW1.py
@@ -17,37 +17,49 @@ def main():
     b_5b = np.array([1,-2,0])
     A_5c = np.array([[2,-2],[-4,3]])
     b_5c = np.array([3,-2])
-    print "(a) x = ",solve_matrix(A_5a,b_5a)
-    print "(b) x = ",solve_matrix(A_5b,b_5b)
-    print "(c) x = ",solve_matrix(A_5c,b_5c)
+    print("(a) x = ")
+    print (solve_matrix(A_5a,b_5a))
+    print("(b) x = ")
+    print (solve_matrix(A_5b,b_5b))
+    print("(c) x = ")
+    print (solve_matrix(A_5c,b_5c))
     print("")
     print("---------------------------------------")
     print("Q6\n")
     #Q6
     A_6 = np.array([[3,2],[1,-2]])
     B_6 = np.array([[-1,-2],[4,-2]])
-    print "(a)"
-    print "A + 2B = \n", A_6+2*B_6
+    print ("(a)")
+    print ("A + 2B =")
+    print (A_6+2*B_6)
     print("")
-    print "(b)"
-    print "AB = \n", A_6.dot(B_6) 
-    print "BA = \n", B_6.dot(A_6)
+    print ("(b)")
+    print ("AB =")
+    print (A_6.dot(B_6)) 
+    print ("BA =") 
+    print (B_6.dot(A_6))
     print("")
-    print "(c)"
-    print "At = \n", A_6.T
+    print ("(c)")
+    print ("At =")
+    print (A_6.T)
     print("")
-    print "(d)"
-    print "BB = \n", B_6.dot(B_6)
+    print ("(d)")
+    print ("BB =")
+    print (B_6.dot(B_6))
     print("")
-    print "(e)"
-    print "AtBt = \n", A_6.T.dot(B_6.T)
-    print "(AB)t = \n", (A_6.dot(B_6)).T
+    print ("(e)")
+    print ("AtBt =")
+    print (A_6.T.dot(B_6.T))
+    print ("(AB)t =")
+    print ((A_6.dot(B_6)).T)
     print("")
-    print "(f)"
-    print "det(A) = \n", np.linalg.det(A_6)
+    print ("(f)")
+    print ("det(A) =")
+    print (np.linalg.det(A_6))
     print("")
-    print "(g)"
-    print "B inv = \n", np.linalg.inv(B_6)
+    print ("(g)")
+    print ("B inv =")
+    print (np.linalg.inv(B_6))
     print("")
 
 if __name__ == "__main__":
